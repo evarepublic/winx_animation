324f406d3f22b6f9e4e60f25e930c19b138bdc42
Ever <ever4ever@yandex.ru>
2021-11-24 15:25:06 +0300

    Рекурсивный setTimeout

---
+++ main.js
@@ -0,0 +1,33 @@
+const bloom = document.querySelector(".bloom"),
+  flora = document.querySelector(".flora"),
+  musa = document.querySelector(".musa");
+
+let count = 0,
+  step = 0;
+
+const flyAnimate = function () {
+  count++;
+  flora.style.top = -count / 12 + "px";
+  flora.style.left = count / 3 + "px";
+  if (count < 750) {
+    bloom.style.top = count / 10 + "px";
+    bloom.style.left = count / 5 + "px";
+    musa.style.top = -50 - count / 10 + "px";
+    musa.style.left = 400 - count / 3 + "px";
+  } else if (count === 750) {
+    bloom.style.transition = `4s`;
+    bloom.style.transform = `rotate(-450deg)`;
+    musa.style.transition = `4s`;
+    musa.style.transform = `rotate(-450deg)`;
+  } else if (count > 750) {
+    bloom.style.top = count / -20 + "px";
+    bloom.style.left = count / 2 + "px";
+    musa.style.top = -50 - count / -20 + "px";
+    musa.style.left = 400 - count / 2 + "px";
+  }
+  if (count < 1500) {
+    setTimeout(flyAnimate, 10);
+  }
+};
+
+flyAnimate();
